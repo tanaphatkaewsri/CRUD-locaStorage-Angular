431614400710754946cfb458589ac726e8e29c9d
Tanaphat <tanaphatkaewsri@gmail.com>
2023-09-28 17:29:14 +0700

    update280966

--- crud_localStorage/src/app/data.service.ts
+++ crud_localStorage/src/app/data.service.ts
@@ -1,9 +1,46 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
+  constructor() {}
 
-  constructor() { }
+  getAllItems(): any[] {
+    const items = localStorage.getItem('items');
+    const parse_items = items ? JSON.parse(items) : [];
+
+    return parse_items;
+  }
+
+  // getAllData(): any {
+  //   const allData: any = {};
+  //   for (let i = 0; i < localStorage.length; i++) {
+  //     // console.log(i);
+  //     let key = localStorage.key(i);
+  //     if (key == null) {
+  //       key = 'nothing';
+  //     }
+  //     const value = localStorage.getItem(key);
+  //     allData[key] = value;
+  //   }
+  //   return allData;
+  // }
+
+  addItem(item: any) {
+    const items = this.getAllItems();
+    items.push(item);
+    const filter_items = this.removeDuplicate(items);
+    localStorage.setItem('items', JSON.stringify(filter_items));
+  }
+
+  removeDuplicate(arr: any[]): any[] {
+    return arr.filter((value, index, self) => {
+      return self.indexOf(value) === index;
+    });
+  }
+
+  // addItems(item: string, value: string): void {
+  //   localStorage.setItem(item, value);
+  // }
 }
--- crud_localStorage/src/app/items/items.component.ts
+++ crud_localStorage/src/app/items/items.component.ts
@@ -1,17 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { FormControl, Validators } from '@angular/forms';
+
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-items',
   templateUrl: './items.component.html',
   styleUrls: ['./items.component.css'],
 })
-export class ItemsComponent {
+export class ItemsComponent implements OnInit {
+  constructor(private dataService: DataService) {}
+
+  // items = this.dataService.getAllData();
+
+  items: any[] = [];
+
   item: any = '';
+  check: boolean = true;
   control_item: FormControl = new FormControl('', [Validators.required]);
 
+  ngOnInit(): void {
+    this.items = this.dataService.getAllItems();
+    console.log('before: ', this.items);
+  }
+
   submitForm() {
-    console.log('add item:');
+    // console.log('invalid ', this.control_item.invalid);
+    // console.log('touch ', this.control_item.touched);
+
+    if (!this.control_item.invalid) {
+      this.item = this.control_item.value;
+      console.log('OK, Data is: ', this.item);
+      // this.dataService.addItems('items', this.item);
+      this.dataService.addItem(this.item);
+      this.items = this.dataService.getAllItems();
+      console.log('after: ', this.items);
+
+      this.control_item.setValue('');
+      this.check = true;
+    } else {
+      this.check = false;
+      console.log('Please Fill Data');
+    }
+    // this.dataService.getAllData();
+  }
+
+  updateItem(item: any) {
+    console.log('update click', item);
+    this.control_item.setValue(item);
+  }
+
+  deleteItem(item: any) {
+    console.log('delete click', item);
   }
 }
