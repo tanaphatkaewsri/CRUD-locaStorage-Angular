2e56560c56233a94edf1074e5ca0fde0a5013ce8
Tanaphat <tanaphatkaewsri@gmail.com>
2023-09-29 09:43:13 +0700

    update290966

--- crud_localStorage/src/app/data.service.ts
+++ crud_localStorage/src/app/data.service.ts
@@ -9,23 +9,8 @@ export class DataService {
   getAllItems(): any[] {
     const items = localStorage.getItem('items');
     const parse_items = items ? JSON.parse(items) : [];
-
     return parse_items;
   }
-
-  // getAllData(): any {
-  //   const allData: any = {};
-  //   for (let i = 0; i < localStorage.length; i++) {
-  //     // console.log(i);
-  //     let key = localStorage.key(i);
-  //     if (key == null) {
-  //       key = 'nothing';
-  //     }
-  //     const value = localStorage.getItem(key);
-  //     allData[key] = value;
-  //   }
-  //   return allData;
-  // }
 
   addItem(item: any) {
     const items = this.getAllItems();
@@ -36,11 +21,7 @@ export class DataService {
 
   updateItem(value: any, index: any) {
     let items = this.getAllItems();
-    // console.log('value: ', value);
-    // console.log('index: ', index);
-    // console.log('after update:', items);
     items[index] = value;
-    // console.log('before update:', items);
     const filter_items = this.removeDuplicate(items);
     localStorage.setItem('items', JSON.stringify(filter_items));
   }
@@ -56,8 +37,4 @@ export class DataService {
       return self.indexOf(value) === index;
     });
   }
-
-  // addItems(item: string, value: string): void {
-  //   localStorage.setItem(item, value);
-  // }
 }
--- crud_localStorage/src/app/items/items.component.ts
+++ crud_localStorage/src/app/items/items.component.ts
@@ -25,18 +25,12 @@ export class ItemsComponent implements OnInit {
 
   ngOnInit(): void {
     this.items = this.dataService.getAllItems();
-    // console.log('before: ', this.items);
-    // console.log('access array: ', this.items[0]); // we got him
   }
 
   submitForm() {
-    // console.log('invalid ', this.control_item.invalid);
-    // console.log('touch ', this.control_item.touched);
 
     if (!this.control_item.invalid) {
       this.item = this.control_item.value;
-      // console.log('OK, Data is: ', this.item);
-      // this.dataService.addItems('items', this.item);
       if (this.from_update) {
         this.dataService.updateItem(this.item, this.index);
         this.from_update = false;
@@ -44,28 +38,20 @@ export class ItemsComponent implements OnInit {
         this.dataService.addItem(this.item);
       }
       this.items = this.dataService.getAllItems();
-      // console.log('after: ', this.items);
       this.control_item.setValue('');
       this.check = true;
     } else {
       this.check = false;
-      // console.log('Please Fill Data');
     }
-    // this.dataService.getAllData();
   }
 
   updateClick(item: any, index: any) {
     this.from_update = true;
-    // console.log('update click', index);
     this.control_item.setValue(item);
     this.index = index;
-    // this.item = this.control_item.value;
-    // this.items = this.dataService.getAllItems();
   }
 
   deleteClick(index: any) {
-    // console.log('delete item click', item);
-    // console.log('delete index click', index);
     this.dataService.deleteItem(index);
     this.items = this.dataService.getAllItems();
   }
